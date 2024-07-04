f6a6deac128b688c9ab72afc687bbc2f8d5543ba
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-07-03 23:27:25 -0300

    implementação do código para realizar o rastreamento de encomendas

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function buscarEncomendas(codigo) {
+    let user = 'teste';
+    let token = '1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f';
+    let url = `https://api.linketrack.com/track/json?user=${user}&token=${token}&codigo=${codigo}`;
+
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição da API');
+            }
+            return response.json();
+        })
+        .then(data => {
+            let eventos = data.eventos
+            let codigo = data.codigo
+
+            console.log(codigo);;
+            console.log(eventos);
+
+        })
+        .catch(error => {
+            console.error(error);
+            buscarEncomendas(codigo)
+        })
+}
+    
+const codigo = 'NM479945411BR'
+buscarEncomendas(codigo)
