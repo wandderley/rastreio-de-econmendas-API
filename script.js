0221c6ef333655c899e84f459e612bc472ad93b8
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-07-04 18:25:11 -0300

    atualização do código de rastreio - mapeando o retorno dos dados

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const codigoRastreio = document.querySelector('#codigoRastreio')
 const btnBuscarRastreio = document.querySelector('#buscarRastreio')
 
+
+let dia = []
+let hora = []
+let local = []
+let statusRastreio = []
+let subStatus = []
+
+
 btnBuscarRastreio.addEventListener('click', function () {
-    if(!codigoRastreio){
+    if (!codigoRastreio.value) {
         alert('digite um valor válido')
-    }else{
+    } else {
         const codigo = codigoRastreio.value
         buscarEncomendas(codigo)
     }
+    console.log(dia)
+    console.log(hora)
+    console.log(local)
+    console.log(statusRastreio)
+    console.log(subStatus)
 
 })
 
+
+
+
 function buscarEncomendas(codigo) {
-    // let user = 'teste';
-    // let token = '1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f';
-    // let url = `https://api.linketrack.com/track/json?user=${user}&token=${token}&codigo=${codigo}`;
     let user = 'teste';
     let token = '1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f';
     let url = `https://api.linketrack.com/track/json?user=${user}&token=${token}&codigo=${codigo}`;
@@ -25,14 +38,16 @@ function buscarEncomendas(codigo) {
             let eventos = data.eventos
             let codigo = data.codigo
 
-            console.log(codigo);;
-            console.log(eventos);
-
+            eventos.map((evento) => {
+                dia.push(evento.data)
+                hora.push(evento.hora)
+                local.push(evento.local)
+                statusRastreio.push(evento.status)
+                subStatus.push(evento.subStatus)
+            })
         })
         .catch(error => {
             console.error(error);
             buscarEncomendas(codigo);
         })
 }
-
-//NM479945411BR
